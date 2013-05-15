137d002a0d24faaa99b3451bebffd00416b4d1d4
mark.harrison <mark.harrison@15below.com>
2013-05-15 14:58:05 +0100

    sort error handling

--- caburnuk.co.uk/caburnuk/App_Start/RouteConfig.cs
+++ caburnuk.co.uk/caburnuk/App_Start/RouteConfig.cs
@@ -10,11 +10,10 @@ namespace caburnuk
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
+               "Default",
+               "{action}/{id}",
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
