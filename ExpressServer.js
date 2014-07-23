2a42b537d636cc5bdb4ab8dfb200c4e704197ea7
harrisonmeister <harrisonmeister@gmail.com>
2014-07-23 19:55:13 +0100

    Move to Node.js site

---
+++ ExpressServer.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/'));
+app.configure(function () {
+    app.use(express.bodyParser());
+});
+
+app.use(function (req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404.html');
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-web
-===
-
-web
+caburnuk
+========
